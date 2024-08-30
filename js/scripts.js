4606f6ff3080178cc373649ea59f8a0836b46ee3
Otsuka4 <ktc24a31e0003@edu.kyoto-tech.ac.jp>
2024-08-30 11:43:52 +0900

    コミット

--- js/scripts.js
+++ js/scripts.js
@@ -28,18 +28,5 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slides');
 
-function showSlides() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = 'block';
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
-
-showSlides(); // Initial call
 
